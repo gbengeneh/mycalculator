5a12dd3741981e44e822b665134c6117389ea525
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2025-08-21 17:03:11 +0100

    update calculator component

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,14 +1,78 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
+import { Dimensions, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { FC, useState } from 'react'
 import { Colors } from '@/constants/Colors'
 
-const Calculator = () => {
-  const router = useRouter()
+const {width} = Dimensions.get('window');
+
+
+interface CalculatorButtonProps {
+  title: string;
+  onPress: () => void;
+  color?: string;
+  textColor?: string;
+  size? : 'normal'| 'double' | 'tripple';
+}
+
+const CalculatorButton: React.FC<CalculatorButtonProps> = ({
+  title,
+  onPress,
+  color = Colors.button.default,
+  textColor = Colors.button.text,
+  size = 'normal'
+})=>{
+  const buttonWidth = size === 'double' ? width / 2.5: size === 'tripple' ? width / 1.25: width/ 4.5;
+  return (
+    <TouchableOpacity
+      style= {[styles.button, {backgroundColor: color, width: buttonWidth}]}
+      onPress={onPress}
+    >
+      <Text style={[styles.buttonText, {color: textColor}]}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const Calculator:FC = () => {
+  const [display ,  setDisplay] = useState('0')
+  const [previousValue , setPreviousValue]  = useState<number | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
+  const [waitingForOperand, setWaitingForOperand] = useState(false)
+
+
+  const inputNumber = (num: string)=>{
+    if(waitingForOperand){
+      setDisplay(num);
+      setWaitingForOperand(false);
+    }else{
+      setDisplay(display === '0' ? num : display + num)
+    }
+  };
+
+  const inputDot = () => {
+    if(waitingForOperand){
+      setDisplay('0.');
+      setWaitingForOperand(false);
+    }else if(display.indexOf('.') === -1){
+      setDisplay(display + '.' )
+    }
+  };
+
+  const clear = () =>{
+    setDisplay('0');
+    setPreviousValue(null);
+    setOperator(null)
+    setWaitingForOperand(false)
+  };
+  
   return (
     <View style={styles.container}>
       <View style={styles.displayContainer}>
+        <Text style={styles.diplayText} numberOfLines={1} adjustsFontSizeToFit>
+          0
+        </Text>
+      </View>
 
+      <View style={styles.buttonsContainer}>
+          
       </View>
        
     </View>
